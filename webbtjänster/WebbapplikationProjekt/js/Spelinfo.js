5732b0e48484713eebac94ac7af57e85982ddcd8
Haug3n <tim.haugen@live.se>
2017-10-26 11:11:58 +0200

    Lagt till giantbomb som alternativt API

--- webbtjänster/WebbapplikationProjekt/js/Spelinfo.js
+++ webbtjänster/WebbapplikationProjekt/js/Spelinfo.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     
     var infoURL = "";
+    var name = "";
 
     function MyFunction(){
 
@@ -11,7 +12,7 @@ $(document).ready(function() {
                 if(pair[0] == "varTitle"){
                     name = pair[1];
                     infoURL = "http://thegamesdb.net/api/GetGame.php?name=" + name;
-                   // alert(infoURL);
+                    //alert("first " + name);
                 }
         }
         return(false); 
@@ -38,12 +39,12 @@ $(document).ready(function() {
                     var urlSplit=url.split("=");
                     var completerURL = "https://www.youtube.com/embed/" + urlSplit[1];
 
-                    alert(urlSplit[1]);
+                    //alert(urlSplit[1]);
                     var html_block = 
                     '<iframe id="Trailer" width="854" height="480" src="' + completerURL +'" frameborder="0" gesture="media" allowfullscreen></iframe>';
                     $("#trailerDiv").append(html_block);
 
-                    alert("https://www.youtube.com/embed/" + urlSplit[1]);
+                   // alert("https://www.youtube.com/embed/" + urlSplit[1]);
                     
                     var baseImageUrl = $(this).find("baseImgUrl")
                     $("#GameCover").attr("src", $(this).find("baseImgUrl"));
@@ -60,3 +61,62 @@ $(document).ready(function() {
         });   
     });         
 });
+
+
+////////////////////////////////////////////////////////////
+
+
+var apikey = "3d648576c9b847530e45eb0b1f82b380fd009bf9";
+var baseUrl = "http://www.giantbomb.com/api";
+
+// construct the uri with our apikey
+var GamesSearchUrl = baseUrl + '/search/?api_key=' + apikey + '&format=json&limit=100';
+var query =  GamesSearchUrl + '&query=' + name + '&resources=game';
+//alert(query);
+
+// construct the uri with our apikey
+var GamesSearchUrl = baseUrl + '/search/?api_key=' + apikey + '&format=json';
+//var query = "Dishonored";
+
+$(document).ready(function() {
+
+
+    var query = window.location.search.substring(1);
+    var vars = query.split("?");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == "varTitle"){
+                name = pair[1];
+                //infoURL = "http://thegamesdb.net/api/GetGame.php?name=" + name;
+                //alert("second " + name);
+            }
+    }
+    
+
+    //MyFunction();
+
+    //alert(name);
+      // send off the query
+      $.ajax({
+        url: GamesSearchUrl + '&query=' + encodeURI(name),
+        dataType: "json",
+        success: searchCallback 
+      });
+    
+    
+        // callback for when we get back the results
+        function searchCallback(data) {
+            $('#test').append('Found ' + data.number_of_total_results + ' results for ' + GamesSearchUrl + '&query=' + encodeURI(name));
+            var games = data.results;
+            $.each(games, function(index, game) {
+                $('#test').append('<h1>' + game.name + '</h1>');
+                $('#test').append('<p>' + game.description + '</p>');
+                $('#test').append('<img src="' + game.posters.thumb_url + '" />');
+            });
+        }
+    
+
+});
+
+
+
